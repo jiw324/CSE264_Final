147e263d1c70991a942fc2da8556e539f06d2155
Sunny You <suy224@lehigh.edu>
2022-12-10 22:26:33 -0500

    stuff

--- Server.js
+++ Server.js
@@ -7,10 +7,8 @@ const HOST = "127.0.0.1";
 
 var http = require("http");
 var express = require("express");
-var app = express();
 var path = require("path");
 var publicPath = path.resolve(__dirname, "public");
-app.use(express.static(publicPath));
 
 class Card{
     constructor(id, symbol){
@@ -20,8 +18,9 @@ class Card{
 }
 
 class Player{
-    constructor(id, landlord, hand){
+    constructor(id, name, landlord, hand){
         this.id = id;
+        this.name = name;
         this.landlord = landlord;
         this.hand = hand;
     }
@@ -29,7 +28,8 @@ class Player{
 
 let cardList = [];
 let playerHand = [];
-let playerCount = 3;
+let playerName = [];
+let playerCount = 0;
 const cardranking = new Map();
 
 function generatePlayer(){
@@ -39,18 +39,18 @@ function generatePlayer(){
     if(playerCount === 3){
         let landlord = Math.floor(Math.random() * (2 - 0) + 0);
         shuffle(cardList);
-        playerHand[0] = new Player(0, landlord === 0 ? true : false, cardList.slice(0, 17));
-        playerHand[1] = new Player(1, landlord === 1 ? true : false, cardList.slice(18, 35));
-        playerHand[2] = new Player(2, landlord === 2 ? true : false, cardList.slice(36, 53));
+        playerHand[0] = new Player(0, playerName[0], landlord === 0 ? true : false, cardList.slice(0, 17));
+        playerHand[1] = new Player(1, playerName[1], landlord === 1 ? true : false, cardList.slice(18, 35));
+        playerHand[2] = new Player(2, playerName[2], landlord === 2 ? true : false, cardList.slice(36, 53));
     }
 }
 
 function generateCard(){
-    cardList[0] = new Card(0, 14); // Red Joker
-    cardList[1] = new Card(1, 15); // Black Joker
+    cardList[0] = new Card(0, 14); // black Joker
+    cardList[1] = new Card(1, 15); // red Joker
     if(cardranking.size === 0){
-        cardranking.set(14, 1);
-        cardranking.set(15, 2);
+        cardranking.set(15, 1);
+        cardranking.set(14, 2);
         cardranking.set(2, 3);
         cardranking.set(1, 4);
         cardranking.set(13, 5);
@@ -77,6 +77,7 @@ function generateCard(){
 
 // Do a frequency sort on set before inputting
 function Combination(set){
+    set.sort(function(a,b){return b - a});
     switch(set.length){
         case(1):
             return true;
@@ -98,7 +99,6 @@ function Combination(set){
             if(set[0].symbol === set[1].symbol && set[0].symbol === set[2].symbol && set[0].symbol === set[3].symbol){
                 return "bomb";
             }
-
             break;
         case(5):
             break;
@@ -114,30 +114,45 @@ function Combination(set){
 //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
-
+  
     // While there remain elements to shuffle.
     while (currentIndex != 0) {
-
+  
       // Pick a remaining element.
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-
+  
       // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
-
+  
     return array;
   }
 
+var bodyParser = require('body-parser');
+var app = express();
 
-app.get("/generateHand", (req, res) => {
-    generatePlayer();
+// create application/json parser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+app.use(express.static(publicPath));
+
+
+app.post("/login", (req, res) => {
     header(res);
-    res.write("asd");
-    req.on('end', function () {
-        io.sockets.emit("hand", playerHand);
-    })
+    playerName[playerCount] = req.body.name;
+    if(playerCount === 3) generatePlayer();
+    res.write(JSON.stringify(playerCount++));
+    res.end();
+});
+
+app.get("/submit", (req, res) => {
+    header(res);
+    res.write(Combination(req.body.set));
     res.end();
 });
 
@@ -158,10 +173,9 @@ function updateHand() {
 }
 
 var server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server, {
+const io = require('socket.io')(server, {
   cors: {
-    origin: "*"
+    origin: '*',
   }
 });
 
@@ -176,3 +190,21 @@ io.on('connection',
 );
 
 server.listen(PORT, HOST, () => { console.log(`Server running at http://${HOST}:${PORT}/`); });
+
+
+
+
+
+
+//
+function doAjaxCall(method,  cmd, params, fcn) {
+    $.ajax(
+    "http://127.0.0.1:3000/" + cmd,
+    {
+        type: method,
+        processData: true,
+        data: params,
+        dataType: "json",
+        success: function (result) {  console.log(result) },
+    });
+}
